567feb33f90fa1c6300b5507ff59b2999f8e195d
Mangoo0110 <anik15@cse.pstu.ac.bd>
2026-03-12 18:33:11 +0600

    refactor(product): align usecases with async usecase contract

--- lib/src/feature/product/domain/usecases/get_product_by_id_use_case.dart
+++ lib/src/feature/product/domain/usecases/get_product_by_id_use_case.dart
@@ -1,11 +1,14 @@
 import 'package:async_handler/async_handler.dart';
+import 'package:tag_app/src/core/usecase/usecase.dart';
 import '../entities/product.dart';
 import '../repositories/product_repository.dart';
 
-final class GetProductByIdUseCase {
+final class GetProductByIdUseCase
+    implements AsyncUsecase<Product?, String> {
   const GetProductByIdUseCase(this._repository);
 
   final ProductRepository _repository;
 
+  @override
   AsyncRequest<Product?> call(String id) => _repository.getProductById(id);
 }
